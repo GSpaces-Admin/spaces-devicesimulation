ff1cc0437abeab8c87a83b833706e4771c4f2acf
GSpaces-Admin <52918403+GSpaces-Admin@users.noreply.github.com>
2019-10-08 11:10:32 +0100

    Updated

---
+++ pump-v1-state.js
@@ -0,0 +1,76 @@
+/*global log*/
+/*global updateState*/
+/*global updateProperty*/
+/*jslint node: true*/
+
+"use strict";
+
+// Default state
+var state = {
+    online: true,
+    vibration: 20.0,
+    vibration_unit: "%",
+    running: 1
+};
+
+// Default properties
+var properties = {};
+
+/**
+ * Restore the global state using data from the previous iteration.
+ *
+ * @param previousState device state from the previous iteration
+ * @param previousProperties device properties from the previous iteration
+ */
+function restoreSimulation(previousState, previousProperties) {
+    // If the previous state is null, force a default state
+    if (previousState) {
+        state = previousState;
+    } else {
+        log("Using default state");
+    }
+
+    if (previousProperties) {
+        properties = previousProperties;
+    } else {
+        log("Using default properties");
+    }
+}
+
+/**
+ * Simple formula generating a random value around the average
+ * in between min and max
+ */
+function vary(avg, percentage, min, max) {
+    var value =  avg * (1 + ((percentage / 100) * (2 * Math.random() - 1)));
+    value = Math.max(value, min);
+    value = Math.min(value, max);
+    return value;
+}
+
+/**
+ * Entry point function called by the simulation engine.
+ * Returns updated simulation state.
+ * Device property updates must call updateProperties() to persist.
+ *
+ * @param context             The context contains current time, device model and id
+ * @param previousState       The device state since the last iteration
+ * @param previousProperties  The device properties since the last iteration
+ */
+/*jslint unparam: true*/
+function main(context, previousState, previousProperties) {
+
+    // Restore the global device properties and the global state before
+    // generating the new telemetry, so that the telemetry can apply changes
+    // using the previous function state.
+    restoreSimulation(previousState, previousProperties);
+
+    if (state.running) {
+        // 10 +/- 5%,  Min 0, Max 20
+        state.vibration = vary(20, 5, 10, 30);
+    } else {
+        state.vibration = 0;
+    }
+
+    updateState(state);
+}
